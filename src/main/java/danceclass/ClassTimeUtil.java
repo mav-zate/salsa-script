96a7be9d2b0818d44c05d2d1e05ac73aa9df8f78
Maverick Alzate <maverick.alzate@gmail.com>
2018-10-07 23:05:38 -0400

    Add ClassTimeUtil

--- src/main/java/danceclass/ClassTimeUtil.java
+++ src/main/java/danceclass/ClassTimeUtil.java
@@ -1,4 +1,28 @@
 package danceclass;
 
 public class ClassTimeUtil {
+
+    public static ClassTime getClassEndTime(ClassTime classStartTime, Integer colSpanValue) throws AbnormalClassLength {
+        if (colSpanValue < 2 || colSpanValue > 3) {
+            throw new AbnormalClassLength();
+        }
+
+        ClassTime classEndTime = clone(classStartTime);
+
+        if (colSpanValue == 2) {
+            classEndTime.addHour();
+        } else if (colSpanValue == 3) {
+            classEndTime.addHour();
+            classEndTime.addHalfHour();
+        }
+
+        return classEndTime;
+    }
+
+    private static ClassTime clone(ClassTime original) {
+        Integer hour = original.getHour();
+        Integer minutes = original.getMinutes();
+
+        return new ClassTime(hour, minutes);
+    }
 }
